7435f8b25a5600a53a02eb5051a41cadf917e430
valeriasm2 <vsantanamegia01.cf@iesesteveterradas.cat>
2026-04-20 18:59:14 +0200

    todo en produccion

--- myprojectUxia/react/src/App.jsx
+++ myprojectUxia/react/src/App.jsx
@@ -1,10 +1,11 @@
+// Bypassing cache 1
 import { useEffect, useState } from "react"
 
 function App() {
-  const [mensaje, setMensaje] = useState("")
+  const [mensaje, setMensaje] = useState(" ")
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/test")
+    fetch("/api/test")
       .then(res => res.json())
       .then(data => setMensaje(data.mensaje))
   }, [])
