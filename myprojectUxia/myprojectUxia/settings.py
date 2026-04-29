7bbb1edce1e292ddbf0b580347a70953396fc4a5
valeriasm2 <vsantanamegia01.cf@iesesteveterradas.cat>
2026-04-29 13:45:44 +0200

    arreglando prod

--- myprojectUxia/myprojectUxia/settings.py
+++ myprojectUxia/myprojectUxia/settings.py
@@ -120,6 +120,8 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://uxiaweb3.ieti.site',
+    'https://uxiaweb3.ieti.site',
 ]
 
 # Ollama URL (si lo usas)
--- myprojectUxia/react/src/pages/AdminLogin.jsx
+++ myprojectUxia/react/src/pages/AdminLogin.jsx
@@ -75,7 +75,7 @@ const AdminLogin = () => {
                 id="username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition"
+                className="text-gray-900 bg-white w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition"
                 placeholder="Ingresa tu usuario"
                 required
                 disabled={loading}
@@ -95,7 +95,7 @@ const AdminLogin = () => {
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition"
+                className="text-gray-900 bg-white w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition"
                 placeholder="Ingresa tu contraseña"
                 required
                 disabled={loading}
